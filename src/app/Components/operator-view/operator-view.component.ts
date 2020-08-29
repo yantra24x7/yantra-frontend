2cfb9335cdb76bb3f818034bf92fe961c452f2a5
priyadharsini <iambpriyadharsini@gmail.com>
2020-08-29 09:53:57 +0530

    changeslvop

--- src/app/Components/operator-view/operator-view.component.ts
+++ src/app/Components/operator-view/operator-view.component.ts
@@ -16,287 +16,272 @@ export class OperatorViewComponent implements OnInit {
   ngOnInit() {
 
 
-//     this.http.get('http://192.168.1.160:3000/rounds').subscribe(res =>{
-//         console.log(res);
-//         this.machine_response = res;
+    this.http.get('http://192.168.1.160:3000/rounds').subscribe(res =>{
+        console.log(res);
+        this.machine_response = res;
+        console.log(res[0].content);
+        console.log(res[0].part)
+     
+      //   Highcharts.chart('container3', {
+      //     chart: {
+      //         type: 'column',
+      //         height: 180
+      //     },
+      //     exporting: {
+      //       enabled: false
+      //     },
+      //     credits: {
+      //       enabled: false
+      //     },
+      //     title: {
+      //         text: ''
+      //     },
+      //     subtitle: {
+      //         text: ''
+      //     },
+      //     xAxis: {
+      //         type: 'category',
+      //         labels: {
+      //             rotation: -45,
+      //             style: {
+      //                 fontSize: '13px',
+      //                 fontFamily: 'Verdana, sans-serif'
+      //             }
+      //         }
+      //     },
+      //     yAxis: {
+      //         min: 0,
+      //         title: {
+      //             text: 'Percentage'
+      //         }
+      //     },
+      //     legend: {
+      //         enabled: false
+      //     },
+      //     tooltip: {
+      //         pointFormat: 'Utilization: <b>{point.y:.1f} millions</b>'
+      //     },
+      //     series: [{
+      //         name: 'Utilization',
+      //          color: "#C91A0D",
+      //          type: undefined,
+      //         data: res[0].content,
+      //         dataLabels: {
+      //             enabled: false,
+      //             rotation: -90,
+      //             color: '#FFFFFF',
+      //             align: 'right',
+      //             format: '{point.y:.1f}', // one decimal
+      //             y: 10, // 10 pixels down from the top
+      //             style: {
+      //                 fontSize: '13px',
+      //                 fontFamily: 'Verdana, sans-serif'
+      //             }
+      //         }
+      //     }]
+      // });
+    
+      //   Highcharts.chart('container4', {
+      //     chart: {
+      //         type: 'column',
+      //         height: 180
+      //     },
+      //     exporting: {
+      //       enabled: false
+      //     },
+      //     credits: {
+      //       enabled: false
+      //     },
+      //     title: {
+      //         text: ''
+      //     },
+      //     subtitle: {
+      //         text: ''
+      //     },
+      //     xAxis: {
+      //         type: 'category',
+      //         labels: {
+      //             rotation: -45,
+      //             style: {
+      //                 fontSize: '13px',
+      //                 fontFamily: 'Verdana, sans-serif'
+      //             }
+      //         }
+      //     },
+      //     yAxis: {
+      //         min: 0,
+      //         title: {
+      //             text: 'Percentage'
+      //         }
+      //     },
+      //     legend: {
+      //         enabled: false
+      //     },
+      //     tooltip: {
+      //         pointFormat: 'Parts: <b>{point.y:.1f} millions</b>'
+      //     },
+      //     series: [{
+      //         name: 'Parts',
+      //          color: "#C91A0D",
+      //          type: undefined,
+      //         data: res[0].part,
+                 
+      //         dataLabels: {
+      //             enabled: false,
+      //             rotation: -90,
+      //             color: '#FFFFFF',
+      //             align: 'right',
+      //             format: '{point.y:.1f}', // one decimal
+      //             y: 10, // 10 pixels down from the top
+      //             style: {
+      //                 fontSize: '13px',
+      //                 fontFamily: 'Verdana, sans-serif'
+      //             }
+      //         }
+      //     }]
+      // });
 
-//   });
+  });
 
-//     Highcharts.chart('container1', {
-//       chart: {
-//           type: 'column',
-//           height: 180
-//       },
-//       exporting: {
-//         enabled: false
-//       },
-//       credits: {
-//         enabled: false
-//       },
-//       title: {
-//           text: ''
-//       },
-//       subtitle: {
-//           text: ''
-//       },
-//       xAxis: {
-//           type: 'category',
-//           labels: {
-//               rotation: -45,
-//               style: {
-//                   fontSize: '13px',
-//                   fontFamily: 'Verdana, sans-serif'
-//               }
-//           }
-//       },
-//       yAxis: {
-//           min: 0,
-//           title: {
-//               text: 'Percentage'
-//           }
-//       },
-//       legend: {
-//           enabled: false
-//       },
-//       tooltip: {
-//           pointFormat: 'Utilization: <b>{point.y:.1f} millions</b>'
-//       },
-//       series: [{
-//           name: 'Utilization',
-//            color: "#056B2D",
-//            type: undefined,
-//           data: [
-//               ['6AM', 24.2],
-//               ['8AM', 20.8],
-//               ['10AM', 14.9],
-//               ['12PM', 13.7],
-//               ['2PM', 13.1],
-//               ['4PM', 12.7],
-//               ['6PM', 12.4],
-//               ['8PM', 12.2]
-//           ],
-//           dataLabels: {
-//               enabled: false,
-//               rotation: -90,
-//               color: '#FFFFFF',
-//               align: 'right',
-//               format: '{point.y:.1f}', // one decimal
-//               y: 10, // 10 pixels down from the top
-//               style: {
-//                   fontSize: '13px',
-//                   fontFamily: 'Verdana, sans-serif'
-//               }
-//           }
-//       }]
-//   });
+  Highcharts.chart('container1', {
+    chart: {
+        type: 'column',
+        height: 180
+    },
+    exporting: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    title: {
+        text: ''
+    },
+    subtitle: {
+        text: ''
+    },
+    xAxis: {
+        type: 'category',
+        labels: {
+            rotation: -45,
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Percentage'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        pointFormat: 'Utilization: <b>{point.y:.1f} millions</b>'
+    },
+    series: [{
+        name: 'Utilization',
+         color: "#056B2D",
+         type: undefined,
+        data: [
+            ['6AM', 24.2],
+            ['8AM', 20.8],
+            ['10AM', 14.9],
+            ['12PM', 13.7],
+            ['2PM', 13.1],
+            ['4PM', 12.7],
+            ['6PM', 12.4],
+            ['8PM', 12.2]
+        ],
+        dataLabels: {
+            enabled: false,
+            rotation: -90,
+            color: '#FFFFFF',
+            align: 'right',
+            format: '{point.y:.1f}', // one decimal
+            y: 10, // 10 pixels down from the top
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    }]
+});
 
     // container2 the chart
-//     Highcharts.chart('container2', {
-//       chart: {
-//           type: 'column',
-//           height: 180
-//       },
-//       exporting: {
-//         enabled: false
-//       },
-//       credits: {
-//         enabled: false
-//       },
-//       title: {
-//           text: ''
-//       },
-//       subtitle: {
-//           text: ''
-//       },
-//       xAxis: {
-//           type: 'category',
-//           labels: {
-//               rotation: -45,
-//               style: {
-//                   fontSize: '13px',
-//                   fontFamily: 'Verdana, sans-serif'
-//               }
-//           }
-//       },
-//       yAxis: {
-//           min: 0,
-//           title: {
-//               text: 'Percentage'
-//           }
-//       },
-//       legend: {
-//           enabled: false
-//       },
-//       tooltip: {
-//           pointFormat: 'Parts: <b>{point.y:.1f} millions</b>'
-//       },
-//       series: [{
-//           name: 'Parts',
-//            color: "#056B2D",
-//            type: undefined,
-//           data: [
-//               ['6AM', 24.2],
-//               ['8AM', 20.8],
-//               ['10AM', 14.9],
-//               ['12PM', 13.7],
-//               ['2PM', 13.1],
-//               ['4PM', 12.7],
-//               ['6PM', 12.4],
-//               ['8PM', 12.2]
-//           ],
-//           dataLabels: {
-//               enabled: false,
-//               rotation: -90,
-//               color: '#FFFFFF',
-//               align: 'right',
-//               format: '{point.y:.1f}', // one decimal
-//               y: 10, // 10 pixels down from the top
-//               style: {
-//                   fontSize: '13px',
-//                   fontFamily: 'Verdana, sans-serif'
-//               }
-//           }
-//       }]
-//   });
+  //   Highcharts.chart('container2', {
+  //     chart: {
+  //         type: 'column',
+  //         height: 180
+  //     },
+  //     exporting: {
+  //       enabled: false
+  //     },
+  //     credits: {
+  //       enabled: false
+  //     },
+  //     title: {
+  //         text: ''
+  //     },
+  //     subtitle: {
+  //         text: ''
+  //     },
+  //     xAxis: {
+  //         type: 'category',
+  //         labels: {
+  //             rotation: -45,
+  //             style: {
+  //                 fontSize: '13px',
+  //                 fontFamily: 'Verdana, sans-serif'
+  //             }
+  //         }
+  //     },
+  //     yAxis: {
+  //         min: 0,
+  //         title: {
+  //             text: 'Percentage'
+  //         }
+  //     },
+  //     legend: {
+  //         enabled: false
+  //     },
+  //     tooltip: {
+  //         pointFormat: 'Parts: <b>{point.y:.1f} millions</b>'
+  //     },
+  //     series: [{
+  //         name: 'Parts',
+  //          color: "#056B2D",
+  //          type: undefined,
+  //         data: [
+  //             ['6AM', 24.2],
+  //             ['8AM', 20.8],
+  //             ['10AM', 14.9],
+  //             ['12PM', 13.7],
+  //             ['2PM', 13.1],
+  //             ['4PM', 12.7],
+  //             ['6PM', 12.4],
+  //             ['8PM', 12.2]
+  //         ],
+  //         dataLabels: {
+  //             enabled: false,
+  //             rotation: -90,
+  //             color: '#FFFFFF',
+  //             align: 'right',
+  //             format: '{point.y:.1f}', // one decimal
+  //             y: 10, // 10 pixels down from the top
+  //             style: {
+  //                 fontSize: '13px',
+  //                 fontFamily: 'Verdana, sans-serif'
+  //             }
+  //         }
+  //     }]
+  // });
 
-   //Stop
-    // container3 the chart
-    // Highcharts.chart('container3', {
-    //     chart: {
-    //         type: 'column',
-    //         height: 180
-    //     },
-    //     exporting: {
-    //       enabled: false
-    //     },
-    //     credits: {
-    //       enabled: false
-    //     },
-    //     title: {
-    //         text: ''
-    //     },
-    //     subtitle: {
-    //         text: ''
-    //     },
-    //     xAxis: {
-    //         type: 'category',
-    //         labels: {
-    //             rotation: -45,
-    //             style: {
-    //                 fontSize: '13px',
-    //                 fontFamily: 'Verdana, sans-serif'
-    //             }
-    //         }
-    //     },
-    //     yAxis: {
-    //         min: 0,
-    //         title: {
-    //             text: 'Percentage'
-    //         }
-    //     },
-    //     legend: {
-    //         enabled: false
-    //     },
-    //     tooltip: {
-    //         pointFormat: 'Utilization: <b>{point.y:.1f} millions</b>'
-    //     },
-    //     series: [{
-    //         name: 'Utilization',
-    //          color: "#C91A0D",
-    //          type: undefined,
-    //         data: [
-    //             ['6AM', 24.2],
-    //             ['8AM', 20.8],
-    //             ['10AM', 14.9],
-    //             ['12PM', 13.7],
-    //             ['2PM', 13.1],
-    //             ['4PM', 12.7],
-    //             ['6PM', 12.4],
-    //             ['8PM', 12.2]
-    //         ],
-    //         dataLabels: {
-    //             enabled: false,
-    //             rotation: -90,
-    //             color: '#FFFFFF',
-    //             align: 'right',
-    //             format: '{point.y:.1f}', // one decimal
-    //             y: 10, // 10 pixels down from the top
-    //             style: {
-    //                 fontSize: '13px',
-    //                 fontFamily: 'Verdana, sans-serif'
-    //             }
-    //         }
-    //     }]
-    // });
+  //  Stop
   
-      // container4 the chart
-    //   Highcharts.chart('container4', {
-    //     chart: {
-    //         type: 'column',
-    //         height: 180
-    //     },
-    //     exporting: {
-    //       enabled: false
-    //     },
-    //     credits: {
-    //       enabled: false
-    //     },
-    //     title: {
-    //         text: ''
-    //     },
-    //     subtitle: {
-    //         text: ''
-    //     },
-    //     xAxis: {
-    //         type: 'category',
-    //         labels: {
-    //             rotation: -45,
-    //             style: {
-    //                 fontSize: '13px',
-    //                 fontFamily: 'Verdana, sans-serif'
-    //             }
-    //         }
-    //     },
-    //     yAxis: {
-    //         min: 0,
-    //         title: {
-    //             text: 'Percentage'
-    //         }
-    //     },
-    //     legend: {
-    //         enabled: false
-    //     },
-    //     tooltip: {
-    //         pointFormat: 'Parts: <b>{point.y:.1f} millions</b>'
-    //     },
-    //     series: [{
-    //         name: 'Parts',
-    //          color: "#C91A0D",
-    //          type: undefined,
-    //         data: [
-    //             ['6AM', 24.2],
-    //             ['8AM', 20.8],
-    //             ['10AM', 14.9],
-    //             ['12PM', 13.7],
-    //             ['2PM', 13.1],
-    //             ['4PM', 12.7],
-    //             ['6PM', 12.4],
-    //             ['8PM', 12.2]
-    //         ],
-    //         dataLabels: {
-    //             enabled: false,
-    //             rotation: -90,
-    //             color: '#FFFFFF',
-    //             align: 'right',
-    //             format: '{point.y:.1f}', // one decimal
-    //             y: 10, // 10 pixels down from the top
-    //             style: {
-    //                 fontSize: '13px',
-    //                 fontFamily: 'Verdana, sans-serif'
-    //             }
-    //         }
-    //     }]
-    // });
   }
 
 }
