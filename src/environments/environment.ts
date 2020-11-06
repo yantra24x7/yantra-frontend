b38c2b583835c8d199e02ecc39f3fa6d03e2534b
priyadharsini <iambpriyadharsini@gmail.com>
2020-11-06 15:57:21 +0530

    latestip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,8 @@ export const environment = {
  //serverUrl: 'http://13.235.142.89:5000/api/v1/',
     //serverUrl: 'http://192.168.0.237:5000/api/v1/',
    // serverUrl: 'http://13.235.142.89:5000/api/v1/',
-   serverUrl: 'http://182.72.104.66:4456/api/v1/',
+   serverUrl: 'http://15.207.212.223:3000//api/v1/',
+   
 
 };
 /*
